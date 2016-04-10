4d96efd0486bc318b4d8b4700e0146b73deab638
Marcinus <szalek95@gmail.com>
2016-04-10 14:10:29 +0200

    init commit zadanie2

--- main.cpp
+++ main.cpp
@@ -1,188 +1,308 @@
-#include "gnuplot_i.hpp"
-#define GNUPLOT_PATH "C:\\gnuplot\\bin"
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <iomanip>
 #include <cmath>
-
-bool czy_dokladnosc;
-double dokladnosc;
-int liczba_iteracji;
-double wczytany_poczatek_przedzialu, wczytany_koniec_przedzialu;
-double poczatek_przedzialu, koniec_przedzialu, pierwiastek, poprzedni_pierwiastek;
-
-typedef double (*FunkcjaNieliniowa)(double x);
-typedef double (*MetodaLiczenia)(FunkcjaNieliniowa);
 using namespace std;
+int liczbaRownan, liczbaIteracji;
+double dokladnosc;
+bool czyDokladnosc = false, czyMacierzWynikowa = false;
+double** A;
+double** M;
+double* b;
+double* N;
+double* poprzedniaMacierzX;
+double* macierzX;
 
-void wybierz_kryterium_zakonczenia();
-void wczytaj_krance_przedzialu (FunkcjaNieliniowa fun);
-double szacuj_pierwiastek(FunkcjaNieliniowa fun, MetodaLiczenia metodaLiczenia);
-void rysuj_wykres(double pierwiastek_bisekcji, double pierwiastek_siecznych, FunkcjaNieliniowa funkcja);
-double funkcja_trygonometryczna(double x);
-double funkcja_kwadratowa(double x);
-FunkcjaNieliniowa wybierz_funkcje();
-
-double metoda_bisekcji(FunkcjaNieliniowa fun) {
-    return (poczatek_przedzialu + koniec_przedzialu) / 2;
-}
-
-double metoda_siecznych(FunkcjaNieliniowa fun) {
-    return poczatek_przedzialu - fun(poczatek_przedzialu)*(koniec_przedzialu-poczatek_przedzialu)/(fun(koniec_przedzialu)-fun(poczatek_przedzialu));
-}
+void inicjalizacjaMacierzy ();
+void wyborWarunkuZakonczeniaAlgorytmu ();
+void pobierzDaneZPlikuDoMacierzyAOrazB ();
+double* wyliczWartosciMacierzyN ();
+double** wyliczWartosciMacierzyM ();
+bool czyMacierzAZbieznaWiersze ();
+bool czyMacierzAZbieznaKolumny ();
+bool czyMacierzAZbiezna();
+bool czyMacierzAZbieznaDlaKolumn();
+bool czyMacierzAZbieznaDlaWierszy();
+bool czyMacierzAZbieznaPierwiastek();
+bool czySpelnionaDokladnosc (double* poprzedniaMacierzX, double* macierzX);
+void liczWgLiczbyIteracji ();
+void liczWgDokladnosci();
+void wyliczWartosciMacierzyWynikowej ();
+void wyswietlMacierze();
+void wyswietlMacierzDwuwymiarowa (double** macierz);
+void wyswietlMacierz (double* macierz);
 
 int main() {
-    cout.precision(10);
-    Gnuplot::set_GNUPlotPath( GNUPLOT_PATH );
+    cout<<"Podaj liczbe rownan: ";
+    cin>>liczbaRownan;
+    inicjalizacjaMacierzy();
+    wyborWarunkuZakonczeniaAlgorytmu();
+    pobierzDaneZPlikuDoMacierzyAOrazB();
 
-    FunkcjaNieliniowa wybrana_funkcja = wybierz_funkcje();
-    wybierz_kryterium_zakonczenia();
-    wczytaj_krance_przedzialu(wybrana_funkcja);
-
-    cout<<"METODA BISEKCJI:\n";
-    double pierwiastek_bisekcji = szacuj_pierwiastek(wybrana_funkcja, metoda_bisekcji);
-    cout<<"METODA SIECZNYCH:\n";
-    double pierwiastek_siecznych = szacuj_pierwiastek(wybrana_funkcja, metoda_siecznych);
-
-    rysuj_wykres(pierwiastek_bisekcji, pierwiastek_siecznych, wybrana_funkcja);
+    if(czyMacierzAZbiezna()) {
+        N = wyliczWartosciMacierzyN();
+        M = wyliczWartosciMacierzyM();
+        wyliczWartosciMacierzyWynikowej();
+        wyswietlMacierze();
+    } else {
+        cout<<"Macierz A nie spelnia warunku zbieznosci! :( "<<endl;
+    }
 
     return 0;
 }
-
-double funkcja_trygonometryczna (double x) {
-    return sin(x);
+bool czyMacierzAZbiezna()
+{
+    return czyMacierzAZbieznaDlaKolumn() || czyMacierzAZbieznaDlaWierszy() || czyMacierzAZbieznaPierwiastek();
 }
-double funkcja_kwadratowa(double x) {
-    return (2*x);
-}
-double funkcja_wykladnicza (double x) {
-    return (pow(2, x) - 5);
-}
+void inicjalizacjaMacierzy () {
 
-FunkcjaNieliniowa wybierz_funkcje() {
+    A = new double* [liczbaRownan];
+    M = new double* [liczbaRownan];
+    b = new double [liczbaRownan];
+    N = new double [liczbaRownan];
+    poprzedniaMacierzX = new double [liczbaRownan];
+    macierzX = new double [liczbaRownan];
 
-    cout<<"Wybierz funkcje [1]: "<<endl;
-    cout<<"[1] sin(2*x-4)"<<endl;
-    cout<<"[2] 4*x*x - 3*x + 5"<<endl;
-    cout<<"[3] 2^x - 5"<<endl;
-    int wybor_funkcji;
-    cin>>wybor_funkcji;
-
-    switch (wybor_funkcji) {
-        case 1: return funkcja_trygonometryczna;
-        case 2: return funkcja_kwadratowa;
-        case 3: return funkcja_wykladnicza;
-        default: return funkcja_kwadratowa;
-    }
-
-}
-
-void wybierz_kryterium_zakonczenia() {
-    cout<<"Podaj kryterium zakonczenia programu:"<<endl;
-    cout<<"1) Dokladnosc"<<endl;
-    cout<<"2) Liczba iteracji"<<endl;
-    int kryterium_zakonczenia;
-    cin>>kryterium_zakonczenia;
-    if(kryterium_zakonczenia == 1)
+    for(int i = 0; i < liczbaRownan; i++)
     {
-        czy_dokladnosc = true;
-        cout<<"Podaj dokladnosc: ";
+        A[i] = new double [liczbaRownan];
+        M[i] = new double [liczbaRownan];
+    }
+}
+
+void wyborWarunkuZakonczeniaAlgorytmu () {
+
+    cout<<endl<<"Podaj warunek zakonczenia wykonywania algorytmu: "<<endl;
+    cout<<"[1] Dokladnosc"<<endl;
+    cout<<"[2] Liczba iteracji"<<endl;
+    int wyborWarunkuStopu;
+    cin>>wyborWarunkuStopu;
+    if(wyborWarunkuStopu == 1) {
+        czyDokladnosc = true;
+        cout<<endl<<"Podaj dokladnosc: ";
         cin>>dokladnosc;
+    } else if(wyborWarunkuStopu == 2) {
+        cout<<"Podaj liczbe iteracji: ";
+        cin>>liczbaIteracji;
     }
-    else if (kryterium_zakonczenia == 2)
+}
+
+void pobierzDaneZPlikuDoMacierzyAOrazB () {
+
+    ifstream plik;
+    plik.open("wspolczynnikiDoMacierzy.txt");
+
+    if(plik.good()) {
+        for(int i = 0; i<liczbaRownan; i++) {
+
+            for(int j = 0; j<liczbaRownan; j++) {
+                plik>>A[i][j];
+            }
+
+            plik>>b[i];
+        }
+        plik.close();
+    } else {
+        cout<<"Nie mozna otworzyc pliku!";
+    }
+}
+
+bool czyMacierzAZbieznaDlaWierszy()
+{
+    for(int i = 0; i < liczbaRownan; i++)
     {
-        czy_dokladnosc = false;
-        cout<<"Podaj liczba iteracji: ";
-        cin>>liczba_iteracji;
+        double sumaDlaWiersza = 0;
+        for(int j = 0; j < liczbaRownan; j++)
+        {
+            if(i!=j)
+                sumaDlaWiersza += abs(A[i][j]) / abs(A[i][i]);
+        }
+        if(sumaDlaWiersza >= 1)
+            return false;
     }
-}
-
-bool czy_rowne_znaki (double arg0, double arg1, FunkcjaNieliniowa fun) {
-    //TODO: zobaczyæ co siê stanie jak bêdzie 0.
-    return fun(arg0) * fun(arg1) > 0;
-}
-
-bool wyswietl_komunikat_o_zlych_krancach() {
-    cout<<"Podane wartosci krancow przedzialow nie spelniaja zalozenia"<<endl;
-    cout<<"o roznych znakach wybranej funkcji obliczonych dla jego krancow!"<<endl<<endl;
     return true;
 }
-void wczytaj_krance_przedzialu (FunkcjaNieliniowa fun) {
-    do {
-        cout<<"Podaj poczatek przedzialu: ";
-        cin>>wczytany_poczatek_przedzialu;
-        cout<<"Podaj koniec przedzialu: ";
-        cin>>wczytany_koniec_przedzialu;
-    } while (czy_rowne_znaki(wczytany_poczatek_przedzialu, wczytany_koniec_przedzialu, fun) && wyswietl_komunikat_o_zlych_krancach());
+
+bool czyMacierzAZbieznaDlaKolumn()
+{
+    for(int j = 0; j < liczbaRownan; j++)
+    {
+        double sumaDlaKolumny = 0;
+        for(int i = 0; i < liczbaRownan; i++)
+        {
+            if(j!=i)
+                sumaDlaKolumny += abs(A[i][j]) / abs(A[j][j]);
+        }
+        if (sumaDlaKolumny >= 1)
+            return false;
+    }
+    return true;
 }
 
-void aktualizuj_krance(FunkcjaNieliniowa fun){
-    if( czy_rowne_znaki(poczatek_przedzialu, pierwiastek, fun))
-        poczatek_przedzialu = pierwiastek;
+bool czyMacierzAZbieznaPierwiastek()
+{
+    double suma = 0;
+    for(int i = 0; i < liczbaRownan; i++)
+    {
+        for(int j = 0; j < liczbaRownan; j++)
+        {
+            if(i!=j)
+            {
+                double c =  (abs(A[i][j]) / abs(A[j][j]));
+                suma += c*c;
+            }
+        }
+    }
+    return sqrt(suma) < 1;
+}
+bool czyMacierzAZbieznaWiersze () {
+
+    double* macierzPomocnicza = new double [liczbaRownan];
+    double* macierzDiagonalna = new double [liczbaRownan];
+    for(int i = 0; i < liczbaRownan; i++) {
+        for(int j = 0; j < liczbaRownan; j++) {
+            if(i!=j)
+                macierzPomocnicza[i] += abs(A[i][j]);
+            else
+                macierzDiagonalna[i] = abs(A[i][j]);
+        }
+    }
+
+    for (int i = 0; i < liczbaRownan; i++) {
+        if(macierzDiagonalna[i] < macierzPomocnicza[i])
+            return false;
+    }
+    return true;
+}
+
+bool czyMacierzAZbieznaKolumny () {
+
+    double* macierzPomocnicza = new double [liczbaRownan];
+    double* macierzDiagonalna = new double [liczbaRownan];
+    for(int j = 0; j < liczbaRownan; j++) {
+        for(int i = 0; i < liczbaRownan; i++) {
+            if(j!=i)
+                macierzPomocnicza[j] += abs(A[i][j]);
+            else
+                macierzDiagonalna[j] = abs(A[i][j]);
+        }
+    }
+
+    for (int i = 0; i < liczbaRownan; i++) {
+        if(macierzDiagonalna[i] < macierzPomocnicza[i])
+            return false;
+    }
+    return true;
+}
+double* wyliczWartosciMacierzyN () {
+
+    double* macierzN = new double [liczbaRownan];
+    for(int i = 0; i < liczbaRownan; i++)
+        //N[i] = D[i]^-1 gdzie D to macierz diagonalna (wartosci z glownej przekatnej macierzy A)
+        macierzN[i] = (1/A[i][i]);
+
+    return macierzN;
+}
+
+double** wyliczWartosciMacierzyM () {
+    //  M = -N * (L + U) , gdzie L + U sa to wartosci z macierzy A nad i pod glowna przekatna
+    double** macierzM = new double* [liczbaRownan];
+    for(int i = 0; i < liczbaRownan; i++)
+        macierzM[i] = new double[liczbaRownan];
+
+    for (int i = 0; i < liczbaRownan; i++) {
+        for (int j = 0; j < liczbaRownan; j++) {
+            double wartosc;
+            if (i == j)
+                wartosc = 0;           //dlatego, ze w powyzszym wzorze mamy L + U wiec wartosci na glownej przekatnej maja wynosic 0
+            else
+                wartosc = - (N[i] * A[i][j]);
+            macierzM[i][j] = wartosc;
+        }
+    }
+    return macierzM;
+}
+
+void wyliczWartosciMacierzyWynikowej () {
+
+    if (czyDokladnosc)
+        liczWgDokladnosci();
     else
-        koniec_przedzialu = pierwiastek;
+        liczWgLiczbyIteracji();
 }
-void licz_wg_dokladnosci(FunkcjaNieliniowa fun, MetodaLiczenia metodaLiczenia) {
-    int przeprowadzonychIteracji = 1;
-    pierwiastek = metodaLiczenia(fun);
-    aktualizuj_krance(fun);
+
+void metodaJacobiego () {
+
+    for (int j=0; j < liczbaRownan; j++) {
+            macierzX[j] = N[j]*b[j];
+            for(int k = 0; k < liczbaRownan; k++)
+                macierzX[j] += M[j][k]*poprzedniaMacierzX[k];
+        }
+}
+void liczWgDokladnosci () {
+
+    int liczbaPrzeprowadzonychIteracji = 0;
     do {
-        poprzedni_pierwiastek = pierwiastek;
-        pierwiastek = metodaLiczenia(fun);
-        aktualizuj_krance(fun);
-        przeprowadzonychIteracji++;
-    } while(fabs(pierwiastek - poprzedni_pierwiastek) >= dokladnosc);
+            for (int i=0; i<liczbaRownan; i++)
+                poprzedniaMacierzX[i] = macierzX[i];
 
-    cout<<"przeprowadzonych iteracji: "<<przeprowadzonychIteracji<<endl;
+            metodaJacobiego();
+            liczbaPrzeprowadzonychIteracji++;
+
+    } while (!czySpelnionaDokladnosc(poprzedniaMacierzX, macierzX));
+
+    cout<<"Przeprowadzonych iteracji: "<<liczbaPrzeprowadzonychIteracji<<endl;
 }
-void licz_wg_iteracji(FunkcjaNieliniowa fun, MetodaLiczenia metodaLiczenia) {
-    for( int i = 0; i < liczba_iteracji; i++) {
-        pierwiastek = metodaLiczenia(fun);
-        aktualizuj_krance(fun);
+
+bool czySpelnionaDokladnosc (double* poprzedniaMacierzX, double* macierzX) {
+
+    for(int i = 0; i < liczbaRownan; i++) {
+        if(abs (poprzedniaMacierzX[i] - macierzX[i]) > dokladnosc) {
+            cout<<abs(poprzedniaMacierzX[i] - macierzX[i])<<endl;
+            return false;
+        }
+    }
+    return true;
+}
+void liczWgLiczbyIteracji () {
+
+    for (int i=0; i<liczbaIteracji; i++) {
+        metodaJacobiego();
+        for (int i=0; i<liczbaRownan; i++)
+            poprzedniaMacierzX[i] = macierzX[i];
+    }
+}
+void wyswietlMacierze() {
+
+    cout<<endl<<"MACIERZ A: "<<endl<<endl;
+    wyswietlMacierzDwuwymiarowa(A);
+    cout<<endl<<"MACIERZ B: "<<endl<<endl;
+    wyswietlMacierz(b);
+    cout<<endl<<"MACIERZ N: "<<endl<<endl;
+    wyswietlMacierz(N);
+    cout<<endl<<"MACIERZ M: "<<endl<<endl;
+    wyswietlMacierzDwuwymiarowa(M);
+    cout<<endl<<"Wynik: "<<endl<<endl;
+    czyMacierzWynikowa = true;
+    wyswietlMacierz(macierzX);
+}
+
+void wyswietlMacierzDwuwymiarowa (double** macierz) {
+
+    for(int i=0; i < liczbaRownan; i++) {
+        for(int j = 0; j < liczbaRownan; j++)
+            cout<<setw(8)<<macierz[i][j]<<" ";
+
+        cout<<endl;
     }
 }
 
-double szacuj_pierwiastek(FunkcjaNieliniowa fun, MetodaLiczenia metodaLiczenia) {
-    poczatek_przedzialu = wczytany_poczatek_przedzialu;
-    koniec_przedzialu = wczytany_koniec_przedzialu;
+void wyswietlMacierz (double* macierz) {
 
-    if (czy_dokladnosc) {
-        licz_wg_dokladnosci(fun, metodaLiczenia);
-    } else {
-        licz_wg_iteracji(fun, metodaLiczenia);
+    for(int i=0; i<liczbaRownan; i++){
+        if(czyMacierzWynikowa)
+            cout<<"x"<<i+1<<" = ";
+        cout<<setprecision(5)<<setw(8)<<macierz[i]<<endl;
     }
-    cout<<"pierwiastek: "<<fixed<<pierwiastek<<endl;
-    return pierwiastek;
 }
-
-void rysuj_punkt(double x, double y, string nazwa, Gnuplot &plot) {
-    plot.set_style("points");
-    plot.set_pointsize( 2.0 );
-    vector<double> vecX(1);
-    vector<double> vecY(1);
-    vecX[0] = x;
-    vecY[0] = y;
-    plot.plot_xy( vecX, vecY, nazwa );
-    getchar();
-}
-
-void rysuj_wykres(double pierwiastek_bisekcji, double pierwiastek_siecznych, FunkcjaNieliniowa funkcja){
-    int liczba_probek = 100;
-    Gnuplot main_plot;
-    //main_plot.set_title( "tytul wykresu" );
-    main_plot.set_xlabel( "X" );
-    main_plot.set_ylabel( "Y" );
-    main_plot.set_style( "lines" );
-    main_plot.set_grid();
-    main_plot.set_xrange(wczytany_poczatek_przedzialu, wczytany_koniec_przedzialu);
-    vector<double> x(liczba_probek+1);
-    vector<double> y(liczba_probek+1);
-    double krok = (wczytany_koniec_przedzialu - wczytany_poczatek_przedzialu) / liczba_probek;
-    for (int i=0; i<=liczba_probek; i++) {
-        x[i] = wczytany_poczatek_przedzialu + i*krok;
-        y[i] = funkcja(x[i]);
-    }
-    main_plot.plot_xy( x, y, "przebieg funkcji" );
-    rysuj_punkt(pierwiastek_bisekcji, funkcja(pierwiastek_bisekcji), "Bisekcja", main_plot);
-    rysuj_punkt(pierwiastek_siecznych, funkcja(pierwiastek_siecznych), "Sieczne", main_plot);
-}
-
-
